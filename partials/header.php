d4ec9f77610b1850f8bc48796149636710e6984a
saykle <079165512@mail.ru>
2016-11-14 12:30:10 +0200

    first commit

---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/fonts.css"/>
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
+    <link rel="stylesheet" href="css/header.css"/>
+    <link rel="stylesheet" href="css/classes.css"/>
+    <link rel="stylesheet" href="css/footer.css"/>
+</head>
+<body>
